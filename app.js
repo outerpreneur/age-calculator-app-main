5d39d1cd55ed933b23acf152dd91c019942793ff
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-07-23 18:54:19 +0200

    playing

--- app.js
+++ app.js
@@ -8,17 +8,11 @@ const yearOutput = document.querySelector(".year-output");
 
 const form = document.querySelector("form");
 
-form.addEventListener("submit", (handleSubmit) => {});
-
 const date = new Date();
 
 let day = date.getDate();
 let month = 1 + date.getMonth();
 let year = date.getFullYear();
-
-console.log(day);
-console.log(month);
-console.log(year);
 
 const months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
 
@@ -29,16 +23,16 @@ function validate() {
     const parent = i.parentElement;
     if (!i.value) {
       i.style.borderColor = "red";
-      parent.querySelector("small").innerText = "This field text is required";
+      parent.querySelector("span").innerText = "This field text is required";
       validator = false;
     } else if (monthInput.value > 12) {
       monthInput.style.borderColor = "red";
-      monthInput.parentElement.querySelector("small").innerText =
+      monthInput.parentElement.querySelector("span").innerText =
         "Must be a valid month";
       validator = false;
     } else if (dayInput.value > 31) {
       dayInput.style.borderColor = "red";
-      dayInput.parentElement.querySelector("small").innerText =
+      dayInput.parentElement.querySelector("span").innerText =
         "Must be a valid day";
       validator = false;
     } else {
@@ -52,24 +46,28 @@ function validate() {
 
 function handleSubmit(e) {
   e.preventDefault();
-  if (validate()) {
-    if (dayInput.value > day) {
-      day = day + months[month - 1];
-      month = month - 1;
-    }
-    if (monthInput.value > month) {
-      month = month + 12;
-      year = year - 1;
-    }
+  alert("hello fucker");
+  // console.log(dayInput.value);
+  // if (validate()) {
+  //   if (dayInput.value > day) {
+  //     day = day + months[month - 1];
+  //     month = month - 1;
+  //   }
+  //   if (monthInput.value > month) {
+  //     month = month + 12;
+  //     year = year - 1;
+  //   }
 
-    const d = day - dayInput.value;
-    const m = month - monthInput.value;
-    const y = year - yearInput.value;
+  //   const d = day - dayInput.value;
+  //   const m = month - monthInput.value;
+  //   const y = year - yearInput.value;
 
-    dayOutput.innerHTML = d;
-    monthOutput.innerHTML = m;
-    yearOutput.innerHTML = y;
-  }
+  //   dayOutput.innerHTML = d;
+  //   monthOutput.innerHTML = m;
+  //   yearOutput.innerHTML = y;
+  // }
 }
 
-form.addEventListener("submit", handleSubmit);
+form.addEventListener("submit", () => {
+  alert("yep");
+});
