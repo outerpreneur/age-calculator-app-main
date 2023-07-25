f171df19b30cf8077b245492c5a6dae8a273fe49
Alek Tysklind <91225228+outerpreneur@users.noreply.github.com>
2023-07-25 19:43:14 +0200

    javascript

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-const dayInput = document.querySelector(".day");
-const monthInput = document.querySelector(".month");
-const yearInput = document.querySelector(".year");
+const dayInput = document.querySelector("#day");
+const monthInput = document.querySelector("#month");
+const yearInput = document.querySelector("#year");
+const iconInput = document.querySelector(".icon");
 
 const dayOutput = document.querySelector(".day-output");
 const monthOutput = document.querySelector(".month-output");
@@ -21,23 +22,34 @@ function validate() {
   let validator = true;
   inputs.forEach((i) => {
     const parent = i.parentElement;
+
     if (!i.value) {
-      i.style.borderColor = "red";
+      i.style.borderColor = "hsl(0, 100%, 67%)";
+      parent.querySelector("span").style.color = "hsl(0, 100%, 67%)";
+      parent.querySelector("h2").style.color = "hsl(0, 100%, 67%)";
       parent.querySelector("span").innerText = "This field text is required";
-      validator = false;
-    } else if (monthInput.value > 12) {
-      monthInput.style.borderColor = "red";
-      monthInput.parentElement.querySelector("span").innerText =
-        "Must be a valid month";
       validator = false;
     } else if (dayInput.value > 31) {
       dayInput.style.borderColor = "red";
       dayInput.parentElement.querySelector("span").innerText =
-        "Must be a valid day";
-      validator = false;
+        "Must be a valid day.";
+      dayInput.parentElement.querySelector("h2").style.color =
+        "hsl(0, 100%, 67%)";
+      dayInput.parentElement.querySelector("span").style.color =
+        "hsl(0, 100%, 67%)";
+    } else if (monthInput.value > 12) {
+      i.style.borderColor = "hsl(0, 100%, 67%)";
+      parent.querySelector("span").style.color = "hsl(0, 100%, 67%)";
+      parent.querySelector("h2").style.color = "hsl(0, 100%, 67%)";
+      parent.querySelector("span").innerText = "Must be a valid month";
+    } else if (yearInput.value > 2023) {
+      i.style.borderColor = "hsl(0, 100%, 67%)";
+      parent.querySelector("span").style.color = "hsl(0, 100%, 67%)";
+      parent.querySelector("h2").style.color = "hsl(0, 100%, 67%)";
+      parent.querySelector("span").innerText = "Must be in the past";
     } else {
       i.style.borderColor = "black";
-      parent.querySelector("small").innerText = "This field text is required";
+      parent.querySelector("span").innerText = "";
       validator = true;
     }
   });
@@ -46,28 +58,28 @@ function validate() {
 
 function handleSubmit(e) {
   e.preventDefault();
-  alert("hello fucker");
-  // console.log(dayInput.value);
-  // if (validate()) {
-  //   if (dayInput.value > day) {
-  //     day = day + months[month - 1];
-  //     month = month - 1;
-  //   }
-  //   if (monthInput.value > month) {
-  //     month = month + 12;
-  //     year = year - 1;
-  //   }
+  if (validate()) {
+    const titles = document.querySelectorAll("h2");
+    titles.forEach((title) => {
+      title.style.color = "hsl(0, 1%, 44%)";
+    });
+    if (dayInput.value > day) {
+      day = day + months[month - 1];
+      month = month - 1;
+    }
+    if (monthInput.value > month) {
+      month = month + 12;
+      year = year - 1;
+    }
 
-  //   const d = day - dayInput.value;
-  //   const m = month - monthInput.value;
-  //   const y = year - yearInput.value;
+    const d = day - dayInput.value;
+    const m = month - monthInput.value;
+    const y = year - yearInput.value;
 
-  //   dayOutput.innerHTML = d;
-  //   monthOutput.innerHTML = m;
-  //   yearOutput.innerHTML = y;
-  // }
+    dayOutput.innerHTML = d;
+    monthOutput.innerHTML = m;
+    yearOutput.innerHTML = y;
+  }
 }
 
-form.addEventListener("submit", () => {
-  alert("yep");
-});
+form.addEventListener("submit", handleSubmit);
